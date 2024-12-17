155914e70d717686ec0a2d0e64562a683d87dee7
sorav6269 <soravrathor786@gmail.com>
2024-12-17 19:55:02 +0530

    up

--- models/blog.js
+++ models/blog.js
@@ -1,36 +1,38 @@
-const { status } = require("init")
-const mongoose = require("mongoose")
+const { request } = require("express");
+const mongoose = require("mongoose");
 
-const BlogSchema = mongoose.Schema({
+const BlogSchema = mongoose.Schema(
+  {
     category: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     title: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     description: {
+      type: String,
+      require: true,
+    },
+    image: {
+      public_id: {
         type: String,
-        require: true
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
-    image:{
-        public_id:{
-            type: String,
-            required: true
-        },
-        url:{
-            type: String,
-            required: true
-        },
+    status: {
+      type: String,
+      default: "0",
     },
-    status:{
-        type:String,
-        default:"0"
-    },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true })
+const BlogModel = mongoose.model("blog", BlogSchema);
 
-const BlogModel = mongoose.model('blog', BlogSchema)
-
-module.exports = BlogModel
+module.exports = BlogModel;
--- models/user.js
+++ models/user.js
@@ -1,55 +1,57 @@
-const { status } = require("init")
-const mongoose = require("mongoose")
+const { request } = require("express");
+const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     email: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     password: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     phone: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     address: {
+      type: String,
+      require: true,
+    },
+    image: {
+      public_id: {
         type: String,
-        require: true
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
-    image:{
-        public_id:{
-            type: String,
-            required: true
-        },
-        url:{
-            type: String,
-            required: true
-        },
+    status: {
+      type: String,
+      default: "pending",
     },
-    status:{
-        type:String,
-        default:"pending"
+    role: {
+      type: String,
+      default: "user",
     },
-    role:{
-        type:String,
-        default:"user"
+    token: {
+      type: String,
     },
-    token:{
-type:String
+    is_verified: {
+      type: Number,
+      default: 0,
     },
-    is_verified:{
-        type:Number,
-        default:0
-    },
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true })
+const UserModel = mongoose.model("user", UserSchema);
 
-const UserModel = mongoose.model('user', UserSchema)
-
-module.exports = UserModel
+module.exports = UserModel;
